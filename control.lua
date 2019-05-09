d98c826f075fcbc9d7599988519719a0f0621928
Aleksander Szamałek <aleksander.szamalek@gmail.com>
2019-05-09 16:58:54 +0200

    get_nearest_stop

--- control.lua
+++ control.lua
@@ -6,8 +6,14 @@ function add_commands()
     end)
 
     commands.add_command("showTrains","shows trains",function() 
-        print_table(global.personalTrains)
+        local personal_trains = get_personal_trains()
+        print_personal_trains(personal_trains)
     end)
+
+    commands.add_command("getNearestStop","shows stops",function(t) 
+        game.print(get_nearest_stop(game.get_player(t.player_index)).unit_number)
+    end)
+
 end
 
 script.on_init(function()
@@ -41,8 +47,37 @@ local function remove_entity_from_table(t,entity)
     table.remove( t , to_remove )
 end
 
-local function get_nearest_stop(player)
-    
+function get_personal_trains()
+    local personal_trains = {}
+
+    for key,value in pairs(global.personalLocomotives) do
+        if value.name == 'personal-train' then
+            table.insert( personal_trains, value.train.id ,value.train )
+        end
+    end
+
+    return personal_trains
+end
+
+function print_personal_trains(personal_trains)
+    for key,value in pairs(personal_trains) do
+        game.print(string.format( "<%d> --> '<%d>'", key,value.id ))
+    end
+end
+
+function get_nearest_stop(player)
+    local nearest_stop = global.personalStops[0]
+    local min_distance =  1000000
+
+    for key,value in pairs(global.personalStops) do
+        local distance = math.sqrt( math.pow((math.abs(value.position.x - player.position.x)),2) +math.pow((math.abs(value.position.y - player.position.y)),2))
+        if distance <= min_distance then
+            min_distance = distance
+            nearest_stop = value
+        end
+    end
+
+    return nearest_stop
 end
 
 script.on_event(defines.events.on_built_entity,function(event) 
@@ -71,11 +106,11 @@ end)
 
 script.on_event(defines.events.on_player_mined_entity,function(event)
 
-    if event.item_stack.name == 'personal-train-stop' then
+    if event.entity.name == 'personal-train-stop' then
         remove_entity_from_table(global.personalStops,event.entity)
     end
 
-    if event.item_stack.name == 'personal-train' then
+    if event.entity.name == 'personal-train' then
         remove_entity_from_table(global.personalLocomotives,event.entity)
     end
 
